04acf33e7f7f65bdcffb35b7d5241043afc7d104
Lars Kanis <kanis@comcard.de>
2011-09-18 16:06:44 +0200

    change license of this library from GPLv2 to LGPLv3

--- lib/libusb/compat.rb
+++ lib/libusb/compat.rb
@@ -1,19 +1,17 @@
-#!/usr/bin/env ruby
-#   Ruby bindings to libusb.
+# This file is part of Libusb for Ruby.
 #
-#   This program is free software; you can redistribute it and/or
-#   modify it under the terms of the GNU General Public License as
-#   published by the Free Software Foundation; version 2 of the License.
+# Libusb for Ruby is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#   This program is distributed in the hope that it will be useful, but
-#   WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#   General Public License for more details.
+# Libusb for Ruby is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
 #
-#   You should have received a copy of the GNU General Public License
-#   along with this program; if not, write to the Free Software
-#   Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-#   MA 02111-1307, USA.
+# You should have received a copy of the GNU Lesser General Public License
+# along with Libusb for Ruby.  If not, see <http://www.gnu.org/licenses/>.
 #
 #   This file is for compatibility with ruby-usb and libusb-0.1.
 #
--- test/test_libusb_mass_storage.rb
+++ test/test_libusb_mass_storage.rb
@@ -1,3 +1,18 @@
+# This file is part of Libusb for Ruby.
+#
+# Libusb for Ruby is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Libusb for Ruby is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Libusb for Ruby.  If not, see <http://www.gnu.org/licenses/>.
+#
 # This test requires a connected, but not mounted mass storage device with
 # read/write access allowed. Based on the following specifications:
 #   http://www.usb.org/developers/devclass_docs/usbmassbulk_10.pdf
