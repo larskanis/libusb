12d422641da3fb468eff852d0dba54b2877121cd
Lars Kanis <lars@greiz-reinsdorf.de>
2012-12-20 14:50:44 +0100

    Bump VERSION to 0.3.0

--- lib/libusb.rb
+++ lib/libusb.rb
@@ -14,7 +14,7 @@
 # along with Libusb for Ruby.  If not, see <http://www.gnu.org/licenses/>.
 
 module LIBUSB
-  VERSION = "0.2.2"
+  VERSION = "0.3.0"
 
   require 'libusb/call'
   require 'libusb/constants'
