c0efd5be362f4507aa40ae69d72cbe3b97a88711
Lars Kanis <lars@greiz-reinsdorf.de>
2016-12-09 22:07:36 +0100

    Use derivation of Bundler::GemHelper.
    Two git tags were set per release. This is now fixed.

--- Rakefile
+++ Rakefile
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # -*- ruby -*-
 
-require 'bundler/gem_tasks'
+require 'bundler/gem_helper'
 require 'rubygems/package_task'
 require 'pathname'
 require 'uri'
@@ -9,6 +9,7 @@ require 'ostruct'
 require 'rake/clean'
 require 'rake_compiler_dock'
 require_relative 'ext/libusb_recipe'
+require_relative 'lib/libusb/gem_helper'
 
 task :gem => :build
 task :compile do
@@ -25,40 +26,6 @@ task :travis=>:compile do
 end
 task :default => :test
 
-task "release:tag" do
-  hfile = "History.md"
-  version = LIBUSB::VERSION
-  reldate = Time.now.strftime("%Y-%m-%d")
-  headline = '([^\w]*)(\d+\.\d+\.\d+)([^\w]+)([2Y][0Y][0-9Y][0-9Y]-[0-1M][0-9M]-[0-3D][0-9D])([^\w]*|$)'
-
-  hin = File.read(hfile)
-  hout = hin.sub(/#{headline}/) do
-    raise "#{hfile} isn't up-to-date for version #{version}" unless $2==version
-    $1 + $2 + $3 + reldate + $5
-  end
-  if hout != hin
-    Bundler.ui.confirm "Updating #{hfile} for release."
-    File.write(hfile, hout)
-    sh "git", "commit", hfile, "-m", "Update release date in #{hfile}"
-  end
-
-  Bundler.ui.confirm "Tag release with annotation:"
-  m = hout.match(/(?<annotation>#{headline}.*?)#{headline}/m) || raise("Unable to find release notes in #{hfile}")
-  Bundler.ui.info(m[:annotation].gsub(/^/, "    "))
-  IO.popen(["git", "tag", "--file=-", version], "w") do |fd|
-    fd.write m[:annotation]
-  end
-end
-
-task "release:guard_clean" => "release:tag"
-
-task "release:rubygem_push" => "gem:native" do
-  CrossLibraries.each do |ruby_platform, _|
-    gh = Bundler::GemHelper.new
-    gh.send(:rubygem_push, "pkg/#{gh.gemspec.name}-#{gh.gemspec.version}-#{ruby_platform}.gem")
-  end
-end
-
 task 'gem:native' do
   sh "bundle package"
   RakeCompilerDock.sh <<-EOT
@@ -69,79 +36,16 @@ task 'gem:native' do
   EOT
 end
 
-class CrossLibrary < OpenStruct
-  include Rake::DSL
-
-  def initialize(ruby_platform, host_platform, libusb_dllname)
-    super()
-
-    self.ruby_platform = ruby_platform
-    self.recipe = LibusbRecipe.new
-    recipe.host = host_platform
-    recipe.configure_options << "--host=#{recipe.host}"
-    self.libusb_dll = Pathname.new(recipe.path) + libusb_dllname
-
-    file libusb_dll do
-      recipe.cook
-    end
-
-    task "libusb_dll:#{ruby_platform}" => libusb_dll
-
-    desc 'Cross compile libusb for win32'
-    task :cross => [ "libusb_dll:#{ruby_platform}" ] do |t|
-      spec = Gem::Specification::load("libusb.gemspec").dup
-      spec.platform = Gem::Platform.new(ruby_platform)
-      spec.extensions = []
-
-      # Remove files unnecessary for native gems
-      spec.files -= `git ls-files ext`.split("\n")
-      spec.files.reject!{|f| f.start_with?('ports') }
-      spec_text_files = spec.files.dup
-
-      # Add native libusb-dll
-      spec.files << "lib/#{libusb_dll.basename}"
-
-      # MiniPortile isn't required for native gems
-      spec.dependencies.reject!{|d| d.name=="mini_portile2" }
-
-      # Generate a package for this gem
-      pkg = Gem::PackageTask.new(spec) do |pkg|
-        pkg.need_zip = false
-        pkg.need_tar = false
-        # Do not copy any files per PackageTask, because
-        # we need the files from the platform specific directory
-        pkg.package_files.clear
-      end
-
-      # copy files of the gem to pkg directory
-      file pkg.package_dir_path => spec_text_files do
-        spec_text_files.each do |fn|
-          f = File.join(pkg.package_dir_path, fn)
-          fdir = File.dirname(f)
-          mkdir_p(fdir) if !File.exist?(fdir)
-          rm_f f
-          safe_ln(fn, f)
-        end
-
-        # copy libusb.dll to pkg directory
-        f = "#{pkg.package_dir_path}/lib/#{libusb_dll.basename}"
-        mkdir_p File.dirname(f)
-        rm_f f
-        safe_ln libusb_dll.realpath, f
-      end
-
-      file "lib/#{libusb_dll.basename}" => [libusb_dll]
-    end
-  end
-end
-
 CrossLibraries = [
   ['x86-mingw32', 'i686-w64-mingw32', 'bin/libusb-1.0.dll'],
   ['x64-mingw32', 'x86_64-w64-mingw32', 'bin/libusb-1.0.dll'],
   ['x86-linux', 'i686-linux-gnu', 'lib/libusb-1.0.so'],
   ['x86_64-linux', 'x86_64-linux-gnu', 'lib/libusb-1.0.so'],
-].each do |ruby_platform, host_platform, libusb_dll|
-  CrossLibrary.new ruby_platform, host_platform, libusb_dll
+].map do |ruby_platform, host_platform, libusb_dll|
+  LIBUSB::CrossLibrary.new ruby_platform, host_platform, libusb_dll
 end
+
+LIBUSB::GemHelper.install_tasks
+Bundler::GemHelper.instance.cross_platforms = CrossLibraries.map(&:ruby_platform)
 
 # vim: syntax=ruby
---
+++ lib/libusb/gem_helper.rb
@@ -0,0 +1,147 @@
+# This file is part of Libusb for Ruby.
+#
+# Libusb for Ruby is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Libusb for Ruby is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Libusb for Ruby.  If not, see <http://www.gnu.org/licenses/>.
+
+require "bundler/gem_helper"
+
+module LIBUSB
+  class GemHelper < Bundler::GemHelper
+    attr_accessor :cross_platforms
+
+    def install
+      super
+
+      task "release:guard_clean" => ["release:update_history"]
+
+      task "release:update_history" do
+        update_history
+      end
+
+      task "release:rubygem_push" => ["gem:native"]
+    end
+
+    def hfile
+      "History.md"
+    end
+
+    def headline
+      '([^\w]*)(\d+\.\d+\.\d+)([^\w]+)([2Y][0Y][0-9Y][0-9Y]-[0-1M][0-9M]-[0-3D][0-9D])([^\w]*|$)'
+    end
+
+    def reldate
+      Time.now.strftime("%Y-%m-%d")
+    end
+
+    def update_history
+      hin = File.read(hfile)
+      hout = hin.sub(/#{headline}/) do
+        raise "#{hfile} isn't up-to-date for version #{version}" unless $2==version.to_s
+        $1 + $2 + $3 + reldate + $5
+      end
+      if hout != hin
+        Bundler.ui.confirm "Updating #{hfile} for release."
+        File.write(hfile, hout)
+        Rake::FileUtilsExt.sh "git", "commit", hfile, "-m", "Update release date in #{hfile}"
+      end
+    end
+
+    def tag_version
+      Bundler.ui.confirm "Tag release with annotation:"
+      m = File.read(hfile).match(/(?<annotation>#{headline}.*?)#{headline}/m) || raise("Unable to find release notes in #{hfile}")
+      Bundler.ui.info(m[:annotation].gsub(/^/, "    "))
+      IO.popen(["git", "tag", "--file=-", version_tag], "w") do |fd|
+        fd.write m[:annotation]
+      end
+      yield if block_given?
+    rescue
+      Bundler.ui.error "Untagging #{version_tag} due to error."
+      sh_with_code "git tag -d #{version_tag}"
+      raise
+    end
+
+    def rubygem_push(path)
+      cross_platforms.each do |ruby_platform|
+        super(path.gsub(/\.gem\z/, "-#{ruby_platform}.gem"))
+      end
+      super(path)
+    end
+  end
+
+
+  class CrossLibrary < OpenStruct
+    include Rake::DSL
+
+    def initialize(ruby_platform, host_platform, libusb_dllname)
+      super()
+
+      self.ruby_platform = ruby_platform
+      self.recipe = LibusbRecipe.new
+      recipe.host = host_platform
+      recipe.configure_options << "--host=#{recipe.host}"
+      self.libusb_dll = Pathname.new(recipe.path) + libusb_dllname
+
+      file libusb_dll do
+        recipe.cook
+      end
+
+      task "libusb_dll:#{ruby_platform}" => libusb_dll
+
+      desc 'Cross compile libusb for win32'
+      task :cross => [ "libusb_dll:#{ruby_platform}" ] do |t|
+        spec = Gem::Specification::load("libusb.gemspec").dup
+        spec.platform = Gem::Platform.new(ruby_platform)
+        spec.extensions = []
+
+        # Remove files unnecessary for native gems
+        spec.files -= `git ls-files ext`.split("\n")
+        spec.files.reject!{|f| f.start_with?('ports') }
+        spec_text_files = spec.files.dup
+
+        # Add native libusb-dll
+        spec.files << "lib/#{libusb_dll.basename}"
+
+        # MiniPortile isn't required for native gems
+        spec.dependencies.reject!{|d| d.name=="mini_portile2" }
+
+        # Generate a package for this gem
+        pkg = Gem::PackageTask.new(spec) do |pkg|
+          pkg.need_zip = false
+          pkg.need_tar = false
+          # Do not copy any files per PackageTask, because
+          # we need the files from the platform specific directory
+          pkg.package_files.clear
+        end
+
+        # copy files of the gem to pkg directory
+        file pkg.package_dir_path => spec_text_files do
+          spec_text_files.each do |fn|
+            f = File.join(pkg.package_dir_path, fn)
+            fdir = File.dirname(f)
+            mkdir_p(fdir) if !File.exist?(fdir)
+            rm_f f
+            safe_ln(fn, f)
+          end
+
+          # copy libusb.dll to pkg directory
+          f = "#{pkg.package_dir_path}/lib/#{libusb_dll.basename}"
+          mkdir_p File.dirname(f)
+          rm_f f
+          safe_ln libusb_dll.realpath, f
+        end
+
+        file "lib/#{libusb_dll.basename}" => [libusb_dll]
+      end
+    end
+  end
+end
