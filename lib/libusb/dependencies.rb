29de687b6a74a08924a0ae24658ef92ee3a94791
Lars Kanis <lars@greiz-reinsdorf.de>
2018-05-05 16:51:23 +0200

    Update libusb to 1.0.22

--- lib/libusb/dependencies.rb
+++ lib/libusb/dependencies.rb
@@ -1,7 +1,7 @@
 module LIBUSB
-  LIBUSB_VERSION = ENV['LIBUSB_VERSION'] || '1.0.22-rc1'
+  LIBUSB_VERSION = ENV['LIBUSB_VERSION'] || '1.0.22'
   LIBUSB_SOURCE_URI = "https://github.com/libusb/libusb/releases/download/v#{LIBUSB_VERSION}/libusb-#{LIBUSB_VERSION}.tar.bz2"
-  LIBUSB_SOURCE_SHA1 = '09598cd30a315203ce5bacadc38b3a720bc1c9a8'
+  LIBUSB_SOURCE_SHA1 = '10116aa265aac4273a0c894faa089370262ec0dc'
 
   MINI_PORTILE_VERSION = '~> 2.1'
 end
