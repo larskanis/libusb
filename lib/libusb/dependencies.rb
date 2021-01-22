ff4e7385d29506ab9f2e8c8f3b6ffddddce984c4
Lars Kanis <lars@greiz-reinsdorf.de>
2021-01-22 22:53:38 +0100

    Update to libusb-1.0.24 for binary gems

--- lib/libusb/dependencies.rb
+++ lib/libusb/dependencies.rb
@@ -1,7 +1,7 @@
 module LIBUSB
-  LIBUSB_VERSION = ENV['LIBUSB_VERSION'] || '1.0.22'
+  LIBUSB_VERSION = ENV['LIBUSB_VERSION'] || '1.0.24'
   LIBUSB_SOURCE_URI = "https://github.com/libusb/libusb/releases/download/v#{LIBUSB_VERSION}/libusb-#{LIBUSB_VERSION}.tar.bz2"
-  LIBUSB_SOURCE_SHA1 = '10116aa265aac4273a0c894faa089370262ec0dc'
+  LIBUSB_SOURCE_SHA1 = 'd8d614b538f7c953b6e3b73f1eea5dc70820a7e2'
 
   MINI_PORTILE_VERSION = '~> 2.1'
 end
