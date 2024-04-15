3647d3d36fc5811ff4668f1df147e472f9523f6a
Lars Kanis <lars@greiz-reinsdorf.de>
2024-04-15 15:19:13 +0200

    Update Eventmachine support for ruby-3.x

--- lib/libusb/eventmachine.rb
+++ lib/libusb/eventmachine.rb
@@ -118,7 +118,7 @@ class DevHandle
     include EM::Deferrable
 
     def initialize(opts, dev_handle, transfer_method)
-      dev_handle.send(transfer_method, opts) do |res|
+      dev_handle.send(transfer_method, **opts) do |res|
         EM.next_tick do
           if res.kind_of?(LIBUSB::Error)
             fail res
@@ -134,7 +134,7 @@ class DevHandle
   #
   # @see Context#eventmachine_register
   #   DevHandle#interrupt_transfer
-  def eventmachine_interrupt_transfer(opts={})
+  def eventmachine_interrupt_transfer(**opts)
     eventmachine_transfer(opts, :interrupt_transfer)
   end
 
@@ -151,7 +151,7 @@ class DevHandle
   #
   # @see Context#eventmachine_register
   #   DevHandle#bulk_transfer
-  def eventmachine_bulk_transfer(opts={})
+  def eventmachine_bulk_transfer(**opts)
     eventmachine_transfer(opts, :bulk_transfer)
   end
 
@@ -171,7 +171,7 @@ class DevHandle
   #
   # @see Context#eventmachine_register
   #   DevHandle#control_transfer
-  def eventmachine_control_transfer(opts={})
+  def eventmachine_control_transfer(**opts)
     eventmachine_transfer(opts, :control_transfer)
   end
 
